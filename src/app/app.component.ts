aa60f8582c592aa53cc7618c00ad6a5949bfc0fc
josephwalsh715 <s00222915@atu.ie>
2023-03-08 12:11:21 +0000

    updated html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IOMDBResponse } from './omdbresponse';
+import { OmdbApiService } from './services/omdb-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WP1Angular-projects';
+  title = 'Movie Finder';
+  movieData:IOMDBResponse | undefined;
+  errorMessge:any;
+
+  constructor(private _omdbService:OmdbApiService){ }
+
+  getMovieDetails(movieName:string): boolean {
+    this._omdbService.getMovieData(movieName).subscribe(
+      movieData => {
+        this.movieData=movieData;
+        console.log("Director name : " + this.movieData.Director)
+      }
+    )
+    return false;
+  }
 }
