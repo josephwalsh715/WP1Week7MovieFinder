cb84356869ac041411dc46a021d014b09f37864f
josephwalsh715 <s00222915@atu.ie>
2023-03-08 11:54:19 +0000

    add angular files

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { OmdbApiService } from './services/omdb-api.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [OmdbApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
